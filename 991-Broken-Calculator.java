ce2cd0c2fb7e62a88062b5681737c3c600b25239
Harkeerat9406 <pharkeeratsingh@gmail.com>
2026-07-14 10:50:51 +0530

    [991] [Time Beats: 100.00%] [Memory Beats: 63.50%] - LeetPush

---
+++ 991-Broken-Calculator.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int brokenCalc(int start, int target) {
+        int ops= 0;
+        
+        while(target > start)
+        {
+            if(target%2 == 0)
+                target/= 2;
+            else
+                target++;
+            
+            ops++;
+        }
+
+        ops+= start-target;
+        return ops;
+    }
+}
