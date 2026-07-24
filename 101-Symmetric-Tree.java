cb24b0e70383362fe49ef0cefd5b0dabbf4ff117
Harkeerat9406 <pharkeeratsingh@gmail.com>
2026-07-24 09:35:14 +0530

    [101] [Time Beats: 100.00%] [Memory Beats: 70.75%] - LeetPush

---
+++ 101-Symmetric-Tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean dfs(TreeNode leftTree, TreeNode rightTree)
+    {
+        if(leftTree==null && rightTree==null)
+            return true;
+        
+        if(leftTree==null || rightTree==null)
+            return false;
+        
+        if(leftTree.val!=rightTree.val) 
+            return false;
+        
+        boolean inner= dfs(leftTree.right, rightTree.left);
+        boolean outer= dfs(leftTree.left, rightTree.right);
+
+        return inner && outer;
+    }
+    
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root, root);
+    }
+}
